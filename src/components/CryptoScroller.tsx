c91494a2ce85c638f8c439da99d12794c1fbc5e4
burtjames18-create <burtjames18-create@users.noreply.github.com>
2026-05-15 01:39:42 -0500

    Dial back crypto scroller speed to 1.0

--- src/components/CryptoScroller.tsx
+++ src/components/CryptoScroller.tsx
@@ -61,7 +61,7 @@ export default function CryptoScroller() {
     let paused = false;
     const tick = () => {
       if (!paused && el.scrollWidth > el.clientWidth) {
-        el.scrollLeft += 2.2;
+        el.scrollLeft += 1.0;
         if (el.scrollLeft >= el.scrollWidth / 2) el.scrollLeft = 0;
       }
       raf = requestAnimationFrame(tick);
